10a4c21c45bdbaefb4e5b4d735d690bf82335a91
rocker1904 <11990712+rocker1904@users.noreply.github.com>
2022-02-23 16:13:18 +0000

    Long Ping Command

---
+++ src/commands/LongPingCommand.ts
@@ -0,0 +1,16 @@
+import {SlashCommandBuilder} from '@discordjs/builders';
+import {CommandInteraction} from 'discord.js';
+import Command from './Command';
+
+export default class LongPingCommand implements Command {
+    public slashCommandBuilder = new SlashCommandBuilder()
+        .setName('long-ping')
+        .setDescription('Replies with Pong after a delay!');
+
+    public permissions = [];
+
+    public async execute(interaction: CommandInteraction) {
+        await new Promise((resolve) => setTimeout(resolve, 1500));
+        await interaction.reply('Pong!');
+    }
+}
